35b0f98c6b9fae2f0ee3b85915e7f4aa9d3afd77
hopxrs <7271@trinity.croydon.sch.uk>
2025-03-10 10:43:15 +0000

    Add files via upload

---
+++ variables game test.py
@@ -0,0 +1,14 @@
+name = input("What is your name? ")
+print("Hello", name)
+footyteam = input("What is your favourite football team? ")
+print("Your favourite team is", footyteam)
+age = int(input("What is your age "))
+print("Your age is", age)
+colour = input("What is your favourite colour? ")
+print("My favourite colour is", colour, "to!")
+
+number = int(input("What is your favourite number? "))
+print("My favourite number is", number, "to!")
+
+game = input("What is your favourite game? ")
+print("I don't like",game,"much.")
